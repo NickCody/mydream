d5a34e9b67cf00d712f1f3051d35817ed9443c84
Nick Codignotto <nick.codignotto@gmail.com>
2025-02-16 18:15:11 -0500

    wrong image u sed

--- server/app/transform.py
+++ server/app/transform.py
@@ -133,7 +133,7 @@ def transform_image(input_image: Image.Image, prompt: str, bg_prompt: str, proce
     final_params = config_loader.get_final_parameters()
     
     # resize composite_image to match final_params width/height
-    composite_input_image = input_image.resize((final_params.get("width", processed_width), final_params.get("height", processed_height)), Image.LANCZOS).convert("RGB")
+    composite_input_image = composite_image.resize((final_params.get("width", processed_width), final_params.get("height", processed_height)), Image.LANCZOS)
     
     if FINAL_PIPELINE is not None: 
         print(f"🎭 Final inpaint")
